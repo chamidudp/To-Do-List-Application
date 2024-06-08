4afad4e14d85bbdba60f652059aaed25e8b8ced6
Chamidu Dilshan <chamindu2150@gmail.com>
2024-06-08 15:37:03 +0530

    change file direct

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
