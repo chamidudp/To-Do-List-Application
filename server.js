9d847a6df6cd562acc659c2bed1551a27147a703
Chamidu Dilshan <chamindu2150@gmail.com>
2024-06-08 14:53:09 +0530

    Create API Endpoints

--- server.js
+++ server.js
@@ -27,6 +27,54 @@ db.connect(err => {
     console.log('MySQL connected...');
 });
 
+// Create a new task
+app.post('/tasks', (req, res) => {
+    const { title, description } = req.body;
+    const sql = 'INSERT INTO tasks (title, description) VALUES (?, ?)';
+    db.query(sql, [title, description], (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.status(201).send({ id: result.insertId, title, description, completed: false });
+    });
+});
+
+// Get all tasks
+app.get('/tasks', (req, res) => {
+    const sql = 'SELECT * FROM tasks';
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.status(200).send(results);
+    });
+});
+
+// Update a task
+app.put('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, description, completed } = req.body;
+    const sql = 'UPDATE tasks SET title = ?, description = ?, completed = ? WHERE id = ?';
+    db.query(sql, [title, description, completed, id], (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.status(200).send({ id, title, description, completed });
+    });
+});
+
+// Delete a task
+app.delete('/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'DELETE FROM tasks WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.status(200).send({ id });
+    });
+});
+
 // Basic route
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -36,4 +84,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
- 
